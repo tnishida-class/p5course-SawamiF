f817edf6ba0549c18aaf63c6a7871e078c1540e6
SawamiF <91964699+SawamiF@users.noreply.github.com>
2021-10-25 11:45:46 +0900

    Change2-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -2,20 +2,29 @@
 function setup() {
   const blue = color(0, 51, 160);
   createCanvas(270, 180);
-  // noStroke();
+  noStroke();
   background(255);
 
   let d = height / 9; // 縞1本の太さ
 
   for(let i = 0; i < 9; i++){
-    // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
+    if(i % 2 == 0){
+      fill(blue)
+    }
+    else{
+      fill(255, 255, 255)
+    }
+    // 青と白のしましま
     rect(0, i * d, width, (i + 1) * d);
   }
 
   fill(blue);
-  let size = d * 5;
+  let size = d * 5;//高さ÷9×5
   rect(0, 0, size, size);
+  //左上の青い正方形
 
   fill(255);
-  // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  rect(0, d * 2, d * 5, d)
+  rect(d * 2, 0, d, d * 5)
+  // 白い十字
 }
