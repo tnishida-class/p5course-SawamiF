e170b36c0dafe6c4d595fd47b801dc85ab4478a0
SawamiF <91964699+SawamiF@users.noreply.github.com>
2021-11-13 15:22:59 +0900

    Change5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,16 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love Snoopy", 0, 0, 400);
 }
 
-function balloon(t){
+function balloon(t, s, r, q){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
   fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  rect(s, r, w + p * 2, h + p * 2);
   fill(255);
   text(t, p, h + p);
+  triangle(w - p, h + p * 2, w + p * 2, h + p * 2, w + p, h + p * 2 + p);
 }
